02c998abe01d3bcc73dca043de4c04eae3c9006f
Jeressia Williamson <jeressiajaytv@gmail.com>
2020-01-20 20:18:05 -0600

    wip

---
+++ handoff.ui/src/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <button className="btn btn-danger">My button</button>
+        <div className="hi"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
